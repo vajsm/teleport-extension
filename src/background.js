e0e9e4838b6d87784c644995a01357fd4d2a9d52
vajsm <vajsm@outlook.com>
2021-06-13 23:09:32 +0200

    Listing the windows in a context menu if there are more than 2 windows

--- src/background.js
+++ src/background.js
@@ -5,20 +5,23 @@
 
 var _lastFocusedWindowId = -1;
 
-function createContextMenu(options, onItemClickedHandler) {
+function createContextMenu(options, onItemClicked, onItemCreated) {
 
-    let itemKind = options.itemKind;
     let parentId = options.parentId;
+    let itemId = options.itemId;
+    let itemTitle = options.title ?? chrome.i18n.getMessage(itemId);
+
+    console.log('Adding item', `itemId: ${itemId}`, `parentId: ${parentId}`, `itemTitle: ${itemTitle}`);
 
     chrome.contextMenus.create({
-      id: `contextMenu_${itemKind}`,
-      title: chrome.i18n.getMessage(itemKind),
+      id: itemId,
+      title: itemTitle,
       contexts: ["page", "frame"],
       parentId: parentId,
       onclick(info, tab) {
-          onItemClickedHandler(info, tab);
+          onItemClicked(info, tab);
       } 
-    }, onItemCreatedHandler);
+    }, onItemCreated);
 }
 
 function getAvailableWindows(callback) {
@@ -26,80 +29,46 @@ function getAvailableWindows(callback) {
         "windowTypes": ["normal"]
       }, 
       function(windows) {
-        console.log('in a callback');
         callback(windows);
       }
     );
-
 }
 
-function moveTabToWindow(tabId, windowId) {
-  // chrome.tabs.move(tabId, {
-  //   "index": -1,
-  //   "windowId": ???
-  // })
-}
-
-function onItemCreatedHandler(info, tab) {
-    // todo
-}
-
-function onTeleportExistingHandler(info, tab) {
-    // todo
-    console.log(info)
-    console.log(tab)
-
+function onTeleportExisting(info, tab) {
     getAvailableWindows(function(windows) {
-
-      console.log(windows);
-      console.log("tab id", tab.id);
-
+      // todo: implement the actual id of the target window if there are more than 2 windows
       let [target] = windows.filter(function(item) { return !item.focused; }).slice(-1);
-      console.log("last window:", target);
       chrome.tabs.move(tab.id, {
-       "index": -1,
+       "index": -1, // todo: implement the selection of tab's position in the window
        "windowId": target.id
       }, function(tabs) { console.log("tabs moved", tabs)})
     });
 }
 
-function onTeleportNewHandler(info, tab) {
-    console.log(info)
-    console.log(tab)
-
+function onTeleportNew(info, tab) {
     // todo: get details about the window the tab comes from
     // and open in the very same window (minimized/maximized, size etc.)
 
     chrome.windows.create({
       "url": tab.url,
       "state": info.state
-    }, onWindowCreateHandler);
-
-    chrome.tabs.remove(tab.id, onTabRemovedHandler(tab));
-
-    // todo: maybe it's better to move instead? 
+    }, onWindowCreated);
+    chrome.tabs.remove(tab.id, onTabRemoved(tab));
+    // todo: maybe it's better to move in a single step instead? 
 }
 
-function onWindowCreateHandler(window) {
+function onWindowCreated(window) {
   console.log('window created');
   console.log(window);
 }
 
-function onTabRemovedHandler(tab) {
+function onTabRemoved(tab) {
   console.log('tab closed');
   console.log(tab);
 }
 
-//chrome.runtime.onInstalled.addListener(
-//    createContextMenu({ itemKind:'teleportExisting' }, onTeleportExistingHandler)
-//);
-
-//chrome.runtime.onInstalled.addListener(
-//    createContextMenu({ itemKind:'teleportNew' }, onTeleportNewHandler)
-//);
-
+chrome.runtime.onInstalled.addListener(refreshOptions);
 chrome.runtime.onStartup.addListener(refreshOptions);
-
 
 chrome.windows.onCreated.addListener(function(window) {
     console.log("a window has been added");
@@ -108,16 +77,16 @@ chrome.windows.onCreated.addListener(function(window) {
   }
 )
 
-chrome.windows.onRemoved.addListener(function(windowId) {
-  console.log("a window has been removed");
-  console.log(windowId);
+// chrome.windows.onRemoved.addListener(function(windowId) {
+//   console.log("a window has been removed");
+//   console.log(windowId);
 
-  // there's no need to refresh - focus needs to change on close anyway
-  // unless there's a way to close the window in the background, but I am not aware yet
-  // todo: investigate
-  //refreshOptions();
-  }
-)
+//   // there's no need to refresh - focus needs to change on close anyway
+//   // unless there's a way to close the window in the background, but I am not aware yet
+//   // todo: investigate
+//   //refreshOptions();
+//   }
+// )
 
 chrome.windows.onFocusChanged.addListener(function(windowId) {
   console.log("a window has been focused");
@@ -140,17 +109,37 @@ function refreshOptions() {
 function setContextMenuForWindow(window) {
   getAvailableWindows(function(windows) {
     // always create an option to move to a new window
-    createContextMenu({ itemKind: 'teleportNew' }, onTeleportNewHandler);
+    createContextMenu({ itemId: 'teleportNew' }, onTeleportNew);
 
     if (windows.length == 2) {
       // If there are only two windows, then expose an option to move to the another window
-      createContextMenu({ itemKind: 'teleportAnother' }, onTeleportExistingHandler);
+      createContextMenu({ itemId: 'teleportAnother' }, onTeleportExisting);
     }
     else if (windows.length > 2) {
-      // TODO if there's more, add another level to the context menu, allowing to select in which window to move
-      console.log('i will implement this soon believe me')
+      // If there's more, add another level to the context menu, allowing to select in which window to move
+      createContextMenu({ itemId: 'teleportSelect' },
+      () => {},
+      () => {
+        windows.filter(x => { 
+          return x.id != _lastFocusedWindowId 
+        })
+        .forEach(x => {
+          createContextMenu({ 
+            itemId: `teleportSelect_${x.id}`,
+            parentId: 'teleportSelect',
+            title: getWindowName(x)
+          }, onTeleportExisting);
+        })
+      });
+
     }
   })
+}
+
+function getWindowName(window) {
+  // todo: improve naming based on the selected tab in window
+  // https://developer.chrome.com/docs/extensions/reference/tabs/#method-getSelected
+  return `Window nr: ${window.id}`;
 }
 
 function getFocusedWindow(callback) {
@@ -164,7 +153,7 @@ function getFocusedWindow(callback) {
 
 // TODOs: 
 // - if there's a single tab in the window, this is useless - hide the option to move to a new window
-// - implement listing windows in the context menu (if there are more than two windows)
+// - if there's more than one option in context menu, name it "teleport tab" instead of a default "teleport extension"
 // - implement options page
 //    Allow to control:
 //    * the position where the page should be transferred to (beginning/end)
@@ -175,3 +164,4 @@ function getFocusedWindow(callback) {
 // - texts and translations
 // - icons
 // - packaging
+// todo: handle 'no last focused' error
