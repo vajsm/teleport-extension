43770d6a060daa79fa012e7f787402c1f40b6efe
vajsm <vajsm@outlook.com>
2021-07-08 19:46:33 +0200

    Loading default settings on installation

--- src/background.js
+++ src/background.js
@@ -1,10 +1,20 @@
 const ContextMenu = require('./modules/context-menu.js');
-
-chrome.runtime.onInstalled.addListener(ContextMenu.refresh);
-chrome.runtime.onStartup.addListener(ContextMenu.refresh);
+const Storage = require('./modules/storage.js');
+const ExtensionOptions = require('./modules/options.js');
 
 chrome.tabs.onCreated.addListener(ContextMenu.refresh);
 chrome.tabs.onRemoved.addListener(ContextMenu.refresh);
 
 chrome.windows.onCreated.addListener(ContextMenu.refresh);
 chrome.windows.onFocusChanged.addListener(ContextMenu.refresh);
+
+chrome.runtime.onStartup.addListener(ContextMenu.refresh);
+chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason == "install") {
+        ExtensionOptions.forEach(option => {
+            let defaultOption = option.values.find(x => x.isDefault);
+            Storage.save(option.id, defaultOption.value);
+        });
+    }
+    ContextMenu.refresh();
+});
