cb65d42ea31333c17d9fb94535cf39a29f5407a4
vajsm <vajsm@users.noreply.github.com>
2021-08-06 01:33:49 +0200

    Fixing indentation

--- webpack/config.test.js
+++ webpack/config.test.js
@@ -2,25 +2,25 @@ const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 
 module.exports = {
-  target: 'node',
-  mode: 'none',
-  externals: [nodeExternals()],
-  watch: true,
-  watchOptions: {
+    target: 'node',
+    mode: 'none',
+    externals: [nodeExternals()],
+    watch: true,
+    watchOptions: {
         ignored: "node_modules",
         poll: true
-  },
-  module: {
-    rules: [{
-        test: /\.js$/,
-        include: path.resolve(__dirname, '../tests'),
-        use: 
-        {
-            loader: 'babel-loader',
-            options: {
-                presets: [ "@babel/preset-env", {}]
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            include: path.resolve(__dirname, '../tests'),
+            use: 
+            {
+                loader: 'babel-loader',
+                options: {
+                    presets: [ "@babel/preset-env", {}]
+                }
             }
-        }
-    }]
-  }
+        }]
+    }
 };
